7e30d142f835e1ce13b0920964d66da411b65529
Djoume Salvetti <djoume.salvetti@shopify.com>
2020-03-26 14:56:45 -0400

    Updates

---
+++ precache-manifest.26f13837ee342fa88102709a6ee243e5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1352b54cbac034e75cb23b184ed54f5",
+    "url": "/ottawa-biz-dir/index.html"
+  },
+  {
+    "revision": "f8e4a5abd091b8cc4633",
+    "url": "/ottawa-biz-dir/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "570efad2a57b34476313",
+    "url": "/ottawa-biz-dir/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "f8e4a5abd091b8cc4633",
+    "url": "/ottawa-biz-dir/static/js/2.bc3236e5.chunk.js"
+  },
+  {
+    "revision": "e899412d8dbefb133da0034f43167f5d",
+    "url": "/ottawa-biz-dir/static/js/2.bc3236e5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "570efad2a57b34476313",
+    "url": "/ottawa-biz-dir/static/js/main.1a381f7a.chunk.js"
+  },
+  {
+    "revision": "a3428ee6caffeaba44e3",
+    "url": "/ottawa-biz-dir/static/js/runtime-main.6fa515cb.js"
+  }
+]);
